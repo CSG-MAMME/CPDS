56c13b7ea0ecab02bfdf0f595ed17656d0c70c1e
Carlos Segarra <carlossegarragonzalez@gmail.com>
2019-10-25 11:33:32 +0200

    Updated vector addition

--- cuda/vector_addtion.c
+++ cuda/vector_addtion.c
@@ -10,9 +10,27 @@ __global__ void vec_add_kernel(float *a_d, float *b_d, float *c_d, int n)
 int main()
 {
     int num_devices;
-    cudaGetDeviceCount(&num_devices);
+    float A[N], B[N], C[N];
+    float *dev_a, *dev_b, *dev_c;
 
-    vec_add_kernel<<<ceil(n, 256)>>>(a, b, c, n);
+    cudaMalloc(&dev_a, N * sizeof(float));
+    cudaMalloc(&dev_b, N * sizeof(float));
+    cudaMalloc(&dev_c, N * sizeof(float));
 
+    // Initialize A and B
+
+    cudaMemcpy(dev_a, a, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 DimGrid(ceil(N/256), 1, 1);
+    dim3 DimBlock(256, 1, 1);
+    vec_add_kernel<<<DimGrid, DimBlock>>>(dev_a, dev_b, dev_c, N);
+
+    // Copy back result
+    cudaMemcpy(c, dev_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_c);
     return 0;
 }
